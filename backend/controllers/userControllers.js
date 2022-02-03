f516ac1fa53018e48e40ca0d39dd99e05cbb175d
Cedrick Garcia <icedgarcia@gmail.com>
2022-02-04 01:00:32 +0800

    Wip

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,30 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModels');
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, image } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already Exist');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Error occured');
+  }
+  // res.json({ name, email });
+});
+module.exports = { registerUser };
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
-});
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// });
+// app.get('/api/notes', (req, res) => {
+//   res.json(notes);
+// });
 
 app.get('/api/notes/:id/', (req, res) => {
   const note = notes.find((n) => n._id === req.params.id);
@@ -20,7 +21,7 @@ app.get('/api/notes/:id/', (req, res) => {
 
   res.send(note);
 });
-
+app.use('/api/users', userRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
