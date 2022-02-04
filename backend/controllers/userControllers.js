ce9e789d63cc42dff8b19ad49d26c735c8c31655
Cedrick Garcia <icedgarcia@gmail.com>
2022-02-04 14:31:47 +0800

    User Auth tested in postman

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -34,4 +34,22 @@ const registerUser = asyncHandler(async (req, res) => {
   // res.json({ name, email });
 });
 
-module.exports = { registerUser };
+// LOGIN USER
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      image: user.image,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid Email or password');
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -33,9 +33,9 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // will encrypt password everytime its saved
 userSchema.pre('save', async function (next) {
