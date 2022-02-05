7dce2d25f91259dd07a6c0db6e89ae151efef860
Cedrick Garcia <icedgarcia@gmail.com>
2022-02-05 20:58:29 +0800

    Add CRUD on backend

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -18,5 +18,56 @@ const createNote = asyncHandler(async (req, res) => {
     res.status(201).json(createdNote);
   }
 });
+const getNoteById = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
 
-module.exports = { getNotes, createNote };
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).json({ message: 'Note not found' });
+  }
+
+  // res.json(note);
+});
+
+const updateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    note.title = title;
+    note.content = content;
+    note.category = category;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } else {
+    res.status(404);
+    throw new Error('Note not found');
+  }
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    await note.remove();
+    res.json({ message: 'Note Removed' });
+  } else {
+    res.status(404);
+    throw new Error('Note not Found');
+  }
+});
+
+module.exports = { getNotes, createNote, getNoteById, updateNote, deleteNote };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const { getNotes, createNote } = require('../controllers/noteController');
+const {
+  getNotes,
+  createNote,
+  getNoteById,
+  updateNote,
+  deleteNote,
+} = require('../controllers/noteController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.route('/').get(protect, getNotes);
 router.route('/create').post(protect, createNote);
-// router.route('/:id').get().put().delete;
+router
+  .route('/:id')
+  .get(getNoteById)
+  .put(protect, updateNote)
+  .delete(protect, deleteNote);
 
 module.exports = router;
