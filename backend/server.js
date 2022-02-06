cdf081c4cfce8a5c525c690413b57480accf03c4
Cedrick Garcia <icedgarcia@gmail.com>
2022-02-07 02:05:13 +0800

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,35 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
 const noteRoutes = require('./routes/noteRoutes');
 const userRoutes = require('./routes/userRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const app = express();
+const path = require('path');
+
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-// app.get('/api/notes', (req, res) => {
-//   res.json(notes);
-// });
-
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+//------------------deployment ----------------------
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
+
+//------------------deployment ----------------------
 
 app.use(notFound);
 app.use(errorHandler);
